ff0d6c0529737298e2f2a3df9da2998b2a0a7dd1
Natacha S <natacha.springer@gmail.com>
2018-12-03 09:58:35 -0500

    day3

---
+++ 2018/Day3/day3.rb
@@ -0,0 +1,20 @@
+input = File.read('day3.txt').chomp.split("\n").map do |d|
+    d.match(/#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/)
+  end
+
+hits = Array.new(1000) { Array.new(1000, 0) }
+
+input.map(&:to_a).map { |a| a.map(&:to_i) }.each do |_, _, left, top, width, height|
+    (top..top + height - 1).each do |row|
+        (left..left + width - 1).each do |col|
+            hits[row][col] += 1
+        end
+    end
+end
+
+solution = hits.flatten.count{ |count| count > 1}
+
+puts "Solution for part 1 is #{solution}"
+
+# Part 2
+
